23ec0b312c76b765ceb27f9e9f2e48f9fb167798
Lucas Pereira Fernandes <lucasperfer83@gmail.com>
2026-01-07 22:21:38 -0300

    Add files via upload

---
+++ App3.java
@@ -0,0 +1,27 @@
+package basic;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class App3 {
+    public static void main(String[] args) {
+        var people = List.of(
+                new Person("Pedro", 15),
+                new Person("Manoel", 21),
+                new Person("Joana", 30),
+                new Person("Rita", 29),
+                new Person("Ricardo", 45)
+        );
+
+       people
+               .stream()
+               .map(p -> p.age)
+               .filter(a -> a >= 18)
+               .sorted(Comparator.reverseOrder())
+               .forEach(System.out::println);
+
+    }
+    record Person(String name ,int age ){
+
+    }
+}
