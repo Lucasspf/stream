ed6e03f10050a34611d6c2b0f032565b1d4f4401
Lucas Pereira Fernandes <lucasperfer83@gmail.com>
2026-01-07 22:05:08 -0300

    Add files via upload

---
+++ App2.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.util.List;
+
+public class App2 {
+    public static void main(String[] args) {
+        var people = List.of(
+                new Person("Pedro", 15),
+                new Person("Manoel", 21),
+                new Person("Joana", 30),
+                new Person("Rita", 29),
+                new Person("Ricardo", 45)
+        );
+
+        people
+                .stream()
+    }
+    record Person(String name ,int age ){
+
+    }
+}
